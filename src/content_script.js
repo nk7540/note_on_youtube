f7ba64f45fbd1aa00d93aa5f7221b9d3d76f8a95
tinmanCtr <ryo.prfm27@gmail.com>
2019-10-21 22:59:50 +0900

    bootstrapで骨組み作成

--- src/content_script.js
+++ src/content_script.js
@@ -1,11 +1,39 @@
 ﻿//----------------------
 $(function () {
-    //↓疎通確認コード：タイトルの■が回転するはず
-    console.log('Chrome拡張の疎通確認コードが動いています。動作確認ができたら削除してください。');
-    let flag = false;
-    setInterval(function () {
-        document.title = (flag ? "■" : "◆") + document.title.replace(/^[■◆]/, '');
-        flag = !flag;
-    }, 1000);
-    //↑疎通確認コード
+    $('#related').prepend(`
+        <div id="notes" class="container">
+            <div class="table-responsive">
+                <table class="table table-striped">
+                    <thead>
+                        <tr>
+                            <th>再生時間</th>
+                            <th>メモ</th>
+                        </tr>
+                    </thead>
+                    <tbody id="notes-tbody"></tbody>
+                </table>
+            </div>
+        </div>
+        <div class="input-group mb-3">
+            <div class="input-group-prepend">
+                <span class="input-group-text" id="basic-addon1">再生時間</span>
+            </div>
+            <input id="note-input" type="text" class="form-control" placeholder="メモを追加" aria-label="Username" aria-describedby="basic-addon1">
+            <div class="input-group-append">
+                <button id="add-note" class="btn btn-outline-secondary" type="button">追加</button>
+            </div>
+        </div>
+    `);
+
+    const input = $('#note-input');
+
+    $('#add-note').on('click', function(e){
+        $('#notes-tbody').append(`
+            <tr>
+                <th scope="row">再生時間</th>
+                <td>${input.val()}</td>
+            </tr>
+        `);
+        input.val('');
+    });
 });
