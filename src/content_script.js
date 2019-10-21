0eb17aa9a3037304bde6ce5a8c03a436286fcabf
tinmanCtr <ryo.prfm27@gmail.com>
2019-10-22 04:19:47 +0900

    大まかな機能実装

--- src/content_script.js
+++ src/content_script.js
@@ -3,11 +3,11 @@ $(function () {
     $('#related').prepend(`
         <div id="notes" class="container">
             <div class="table-responsive">
-                <table class="table table-striped">
+                <table class="table table-striped table-fixed" style="width: 100%;">
                     <thead>
-                        <tr>
-                            <th>再生時間</th>
-                            <th>メモ</th>
+                        <tr class="d-flex">
+                            <th class="col-3">再生時間</th>
+                            <th class="col-9">メモ</th>
                         </tr>
                     </thead>
                     <tbody id="notes-tbody"></tbody>
@@ -16,7 +16,7 @@ $(function () {
         </div>
         <div class="input-group mb-3">
             <div class="input-group-prepend">
-                <span class="input-group-text" id="basic-addon1">再生時間</span>
+                <span class="input-group-text" id="current-time"></span>
             </div>
             <input id="note-input" type="text" class="form-control" placeholder="メモを追加" aria-label="Username" aria-describedby="basic-addon1">
             <div class="input-group-append">
@@ -25,15 +25,37 @@ $(function () {
         </div>
     `);
 
+    const video = $('video').get(0);
     const input = $('#note-input');
+    var currentTime;
+
+    input.on('focus', function(e){
+        video.pause();
+        currentTime = Math.floor(video.currentTime);
+        $('#current-time').text(`${Math.floor(currentTime / 60)}:${currentTime % 60}`)
+    });
 
     $('#add-note').on('click', function(e){
         $('#notes-tbody').append(`
-            <tr>
-                <th scope="row">再生時間</th>
-                <td>${input.val()}</td>
+            <tr class="d-flex">
+                <th class="col-3" scope="row">
+                    <a class="timestamps yt-simple-endpoint" href="/watch?v=${getParameterByName('v')}&t=${currentTime}s">
+                        ${Math.floor(currentTime / 60)}:${currentTime % 60}
+                    </a>
+                </th>
+                <td class="col-9">${input.val()}</td>
             </tr>
         `);
         input.val('');
+        video.play();
+    });
+
+    $(document).on('click', '.timestamps', function(e){
+        e.preventDefault();
+        const time = event.target.href.match(/t=\d+/g);
+        video.currentTime = time[0].substring(2);
+        if (video.paused) {
+            video.play();
+        };
     });
 });
